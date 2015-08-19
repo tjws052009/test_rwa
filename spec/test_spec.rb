794a0229926fa3a6576036a65e2d3de4d5243dbc
lancers078-01 <sugimoto.tomohiro@lancers.co.jp>
2015-08-19 11:06:10 +0900

    fix

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper'
 RSpec.describe "hoge" do
   context 'hoge' do
     it "true or false" do
-      expect(true).to be false
-      # expect(false).to be false
+      # expect(true).to be false
+      expect(false).to be false
     end
   end
 end
