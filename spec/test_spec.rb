6d39b3eef3958913d1f5027885de5689c2a130db
lancers078-01 <sugimoto.tomohiro@lancers.co.jp>
2015-08-19 11:04:46 +0900

    fix

---
+++ spec/test_spec.rb
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+require 'spec_helper'
+
+RSpec.describe "hoge" do
+  context 'hoge' do
+    it "true or false" do
+      expect(true).to be false
+      # expect(false).to be false
+    end
+  end
+end
+
